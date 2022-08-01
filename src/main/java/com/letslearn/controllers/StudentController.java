3eeb6a99d99d436628f15a8878edf830968b80d4
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-08-01 14:59:32 -0700

    helloWorld

---
+++ src/main/java/com/letslearn/controllers/StudentController.java
@@ -0,0 +1,20 @@
+package com.letslearn.controllers;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class StudentController {
+@RequestMapping("/")
+public String index() {
+	return "index.jsp";
+}
+}
