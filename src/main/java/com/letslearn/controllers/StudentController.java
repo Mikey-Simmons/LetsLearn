0bfb8b4240d8708f02753c59105b26a2b2209387
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-08-09 11:28:14 -0700

    Update StudentController.java

--- src/main/java/com/letslearn/controllers/StudentController.java
+++ src/main/java/com/letslearn/controllers/StudentController.java
@@ -11,11 +11,32 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.letslearn.services.StudentService;
+import com.letslearn.models.Student;
 @SuppressWarnings("unused")
 @Controller
+
 public class StudentController {
+	private final StudentService studentService;
+	
+	public StudentController(StudentService studentService) {
+		this.studentService=studentService;
+	}
 @RequestMapping("/")
 public String index() {
 	return "index.jsp";
+}
+@RequestMapping(value="/login", method=RequestMethod.POST)
+public String loginStudent(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+	boolean isAuthenticated = studentService.authenticateStudent(email, password);
+	if(isAuthenticated) {
+		Student u = studentService.findByEmail(email);
+		session.setAttribute("studentId", u.getId());
+		return "redirect:/home";
+	}
+	else {
+		model.addAttribute("error", "Email/Password is incorrect.  Please register if you do not have an account.");
+		return "index.jsp";	
+	}
 }
 }
